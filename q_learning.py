f103cbabe7937460c496aa945b4f605c214780d7
Lokesh Chandra Das <ldas@mathsci-h2wg319lq6nw.uom.memphis.edu>
2023-05-20 17:35:45 -0500

    exploit method added

--- q_learning.py
+++ q_learning.py
@@ -16,3 +16,11 @@ class QLearning:
         """
         self.q_table[s][a] = self.q_table[s][a]+lr*(r+d_f*np.max(self.q_table[n_s])-self.q_table[s][a])
         return self.q_table
+
+    def exploit(self, state):
+        """
+        Return an optimal action based on current states
+        :param state: current observation from the environment
+        :return: current optimal action
+        """
+        return np.argmax(self.q_table[state])
