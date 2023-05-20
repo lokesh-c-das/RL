b047ed12b0db43a6a1b7ca459195a8bf7d294509
Lokesh Chandra Das <ldas@mathsci-h2wg319lq6nw.uom.memphis.edu>
2023-05-20 17:19:21 -0500

    Environment and Q Learning

---
+++ environment.py
@@ -0,0 +1,47 @@
+import numpy as np
+import gym
+import random
+import math
+
+from sklearn.preprocessing import KBinsDiscretizer
+import time
+from typing import Tuple
+import tensorflow as tf
+import datetime
+
+
+class Environment:
+    def __int__(self, algo=None, render_mode=None):
+        self.algo = algo
+        self.env = gym.make("CartPole-v1", render_mode=render_mode)
+        self.num_of_action = self.env.action_space.n  # number of actions in the environment
+        self.num_of_observation = self.env.observation_space.shape[0]  # number of observation in the environment
+        self.observation_space = self.env.observation_space
+        self.action_space = self.env.action_space
+        self.state_bins = (3, 6, 6, 12)
+
+    def state_space_to_discrete(self, position, velocity, angle, angular_velocity) -> tuple[int, ...]:
+        """Convert continuous state space into a discrete state for Q-Learning"""
+        upper_bound = [self.env.observation_space.high[0], 20.0, self.env.observation_space.high[2], math.radians(50)]
+        lower_bound = [self.env.observation_space.low[0], -20.0, self.env.observation_space.high[2], -math.radians(50)]
+        binDis = KBinsDiscretizer(n_bins=self.state_bins, encode="original", strategy="uniform")
+        binDis.fit([lower_bound, upper_bound])
+        binT = binDis.transform([[position, velocity, angle, angular_velocity]])
+        return tuple(map(int, binT[0]))
+
+    def env_reset(self):
+        observation = self.env.reset()
+        if self.algo == "Q-Learning":
+            return self.state_space_to_discrete(*observation)
+        return observation
+
+    def env_step(self, action):
+        next_obs, reward, terminated, done, info = self.env.step(action)
+        if self.algo == "Q-Learning":
+            next_obs = self.state_space_to_discrete(*next_obs)
+        return next_obs, reward, terminated, done, info
+
+    def env_close(self):
+        self.env.close()
+
+
---
+++ q_learning.py
@@ -0,0 +1,18 @@
+import numpy as np
+class QLearning:
+    def __int__(self, state_bins, num_action=2):
+        self.q_table = np.zeros(state_bins, (num_action,))
+
+    def learn_environment(self, s, a, r, n_s, lr,d_f):
+        """
+        Learning the environment to balance the CartPole
+        :param s: current state
+        :param a: action
+        :param r: reward
+        :param n_s: next state
+        :param lr: learning rate
+        :param d_f: discount factor
+        :return: q_table
+        """
+        self.q_table[s][a] = self.q_table[s][a]+lr*(r+d_f*np.max(self.q_table[n_s])-self.q_table[s][a])
+        return self.q_table
